a7fdac39ab5e3060e74b7e8d0a834ed41b26ec6c
thebestusernameever <ajay@ajayinkingston.com>
2017-11-20 09:27:39 -0500

    added basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.D)) {
+            transform.Translate(new Vector3(1, 0));
+        }else if (Input.GetKeyDown(KeyCode.A)) {
+            transform.Translate(new Vector3(-1, 0));
+        }else if (Input.GetKeyDown(KeyCode.W)) {
+            transform.Translate(new Vector3(0, 1));
+        }else if (Input.GetKeyDown(KeyCode.S)) {
+            transform.Translate(new Vector3(0, -1));
+        }
+    }
+}

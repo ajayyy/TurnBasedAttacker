5570a373c3c06c32c3d0625fcb050d7e8c17f3c7
thebestusernameever <ajay@ajayinkingston.com>
2017-12-19 10:27:33 -0500

    Made amount chooser for main menu

---
+++ Assets/Scripts/UI/AmountChooser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AmountChooser : MonoBehaviour {
+
+    public Text count;
+
+    public int min = 1;
+    public int max = 99;
+
+    void Start () {
+		
+	}
+	
+    public void IncreaseCount() {
+        int newNum = int.Parse(count.text) + 1;
+        if (newNum < min) newNum = min;
+        if (newNum > max) newNum = max;
+        count.text = newNum + "";
+    }
+
+    public void DecreaseCount() {
+        int newNum = int.Parse(count.text) - 1;
+        if (newNum < min) newNum = min;
+        if (newNum > max) newNum = max;
+        count.text = newNum + "";
+    }
+}

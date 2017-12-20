454aa22f662ccf49a1ec6520a8a2e36bcb45b4d5
thebestusernameever <ajay@ajayinkingston.com>
2017-12-20 10:06:40 -0500

    Added a static file called GameSettings, and made the maximum start units depend on the amount of players selected.

--- Assets/Scripts/UI/AmountChooser.cs
+++ Assets/Scripts/UI/AmountChooser.cs
@@ -10,11 +10,29 @@ public class AmountChooser : MonoBehaviour {
     public int min = 1;
     public int max = 99;
 
+    //optional, makes the maximum 10x what is in the other text field
+    public Text maxText = null;
+
     void Start () {
 		
 	}
+
+    void Update() {
+        if (maxText != null) {
+            max = 40 / int.Parse(maxText.text);
+
+            if(int.Parse(count.text) > max) {
+                count.text = max + "";
+            }
+        }
+    }
 	
     public void IncreaseCount() {
+
+        if (maxText != null) {
+            max = 60 / int.Parse(maxText.text);
+        }
+
         int newNum = int.Parse(count.text) + 1;
         if (newNum < min) newNum = min;
         if (newNum > max) newNum = max;
---
+++ Assets/Scripts/UI/GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings {
+
+    //number of players playing
+    public static int players = 1;
+
+    //number of units per player
+    public static int units = 1;
+
+}

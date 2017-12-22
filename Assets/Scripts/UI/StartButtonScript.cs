93df5cee50951452d8a6af558afc0724f5c17aa5
thebestusernameever <ajay@ajayinkingston.com>
2017-12-22 12:21:37 -0500

    Saves data for players and start units in static variables and goes to next scene when you hit start. Also added a little pulse animation to the start button

---
+++ Assets/Scripts/UI/StartButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartButtonScript : MonoBehaviour {
+
+    public Text players;
+    public Text units;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void OnClick() {
+        GameSettings.players = int.Parse(players.text);
+        GameSettings.units = int.Parse(units.text);
+
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}

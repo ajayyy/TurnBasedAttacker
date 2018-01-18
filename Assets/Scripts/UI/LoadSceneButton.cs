9924902603973f927123553398c13efd3f9aff7f
Ajay Ramachandran <ajay@ajayinkingston.com>
2018-01-18 12:26:06 -0500

    Added UI for loading games

---
+++ Assets/Scripts/UI/LoadSceneButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneButton : MonoBehaviour {
+
+    public string sceneToLoad = "";
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+    }
+
+    public void OnClick() {
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+}
